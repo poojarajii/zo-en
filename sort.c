c16ffc02b051e71e617e5b9442000be0c15a11c5
poojarajii <36043825+poojarajii@users.noreply.github.com>
2018-03-08 19:50:17 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -10,9 +10,9 @@ int main()
         scanf("%d",&a[i]);
     }
 
-    for(int i=0;i<5;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<5;j++)
+        for(int j=i+1;j<n;j++)
         {
             if(a[i]>a[j])
             {
