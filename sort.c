e6423000d9386aab3f6f6dee966e93d2ec150421
poojarajii <36043825+poojarajii@users.noreply.github.com>
2018-03-08 19:15:14 +0530

    sort.c

---
+++ sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=i+1;j<5;j++)
+        {
+            if(a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("[");
+    for(int i=0;i<n;i++)
+    {
+        
+        printf("\"%d\",",a[i]);
+        
+    }
+    printf("]");
+    return 0;
+}
