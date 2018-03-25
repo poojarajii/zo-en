4df70eccca32bea3db899be89ea9d28ddbd6a37f
poojarajii <36043825+poojarajii@users.noreply.github.com>
2018-03-25 18:38:15 +0530

    table.c

---
+++ table.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() 
+{
+    printf("A\tB\tC\tO/P\n");
+    for(int i=0;i<=1;i++)
+    {
+        for(int j=0;j<=1;j++)
+        {
+            for(int k=0;k<=1;k++)
+            {
+                printf("%d\t%d\t%d\t%d\n",i,j,k,(i|j|k));
+            }
+        }
+    }
+    
+    
+}
